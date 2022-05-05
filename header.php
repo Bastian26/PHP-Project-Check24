a59f67b74da54387139758416da6b60ec69f14f5
Bastian26 <b.struggl@gmx.de>
2022-05-05 10:59:47 +0200

    Project progress

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="banner">
+
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Startseite</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Eintrag hinzufügen</a></li>
+            <li><a href="#">Logout</a></li>
+        </ul>
+    </nav>
+</header>
+
+
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+        <?php
+            include 'header.php'
+        ?>
+
+    <main>
+
+    </main>
+    <footer>
+        <p>© 2007 - Impressum</p>
+    </footer>
+</body>
+</html>
