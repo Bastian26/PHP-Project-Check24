3099f8ab6a4cab3dfe657221da9b0f13c765f518
Bastian26 <b.struggl@gmx.de>
2022-05-05 12:49:48 +0200

    further progress

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -6,20 +6,19 @@ include "../classes/login.classes.php";
 include "../classes/login-contr.classes.php";
 
 
+?>
 
-if ( === true) {
-    ?>
     <div class="index-login-login">
             <h4>LOGIN</h4>
-            <form action="../Check24-Project/includes/login.inc.php" method="post">
+            <form action="login.inc.php" method="post">
                 <input type="text" name="uid" placeholder="Username">
                 <input type="password" name="pwd" placeholder="Password">
                 <br>
                 <button type="submit" name="submit">LOGIN</button>
             </form>
         </div>
+
 <?php
-}
 
 if(isset($_POST["submit"]))
 {
@@ -28,14 +27,15 @@ if(isset($_POST["submit"]))
     $uid = $_POST["uid"];
     $pwd = $_POST["pwd"];
 
-
     $login = new LoginContr($uid, $pwd);
 
     // Running error handlers and user signup
     $login->loginUser();
 
     // Going to back to front page
-    header("location: ../index.php?error=none");
+    header('Location: index.php');
 }
 
 ?>
+
+
